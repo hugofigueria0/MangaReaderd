b24b356c503adb0b12287d7b09913395f54fc03f
hugofigueira0 <hugofigueira0@gmail.com>
2019-03-21 18:07:20 +0000

    Criação dos menus

--- app/src/main/java/pt/ipg/mangareaderd/MainActivity.java
+++ app/src/main/java/pt/ipg/mangareaderd/MainActivity.java
@@ -1,7 +1,10 @@
 package pt.ipg.mangareaderd;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,4 +13,25 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+    public void ZonaDeDestaques(View view){
+        Toast.makeText(this, "Abriu a Zona de Destaques", Toast.LENGTH_LONG).show();
+        Intent intent = new Intent( this, Destaques.class);
+        startActivity(intent);
+
+    }
+    public void ZonaDosFavoritos(View view){
+        Toast.makeText(this, "Abriu a Zona dos Favoritos", Toast.LENGTH_LONG).show();
+        Intent intent = new Intent( this, Favoritos.class);
+        startActivity(intent);
+
+
+    }
+    public void LivrosManga(View view){
+        Toast.makeText(this, "Todos OS Livros da aplicação", Toast.LENGTH_LONG).show();
+        Intent intent = new Intent( this, LivrosManga.class);
+        startActivity(intent);
+
+    }
+
+
 }

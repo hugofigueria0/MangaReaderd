f64f14f4151655e0c81b87679b8d096455a2b5c4
hugofigueira0 <hugofigueira0@gmail.com>
2019-05-31 11:02:02 +0100

    Correção do Trabalho

--- app/src/main/java/pt/ipg/mangareaderd/AdaptadorLivrosManga.java
+++ app/src/main/java/pt/ipg/mangareaderd/AdaptadorLivrosManga.java
@@ -1,10 +1,10 @@
 package pt.ipg.mangareaderd;
 
-import android.support.v7.widget.RecyclerView;
+import androidx.recyclerview.widget.RecyclerView;
 import android.content.Context;
 import android.database.Cursor;
-import android.support.annotation.NonNull;
-import android.support.v7.widget.RecyclerView;
+import androidx.annotation.NonNull;
+
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
--- app/src/main/java/pt/ipg/mangareaderd/FavoritosAlterar.java
+++ app/src/main/java/pt/ipg/mangareaderd/FavoritosAlterar.java
@@ -1,10 +1,9 @@
 package pt.ipg.mangareaderd;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.Toast;
